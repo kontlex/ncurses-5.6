b02df3ff4f08322dbaf81419d20ea0323718572d
Shavyrin Dmitry <Dmitry.Shavyrin@paragon-software.com>
2015-05-12 14:39:42 +0300

    Editing install_script step 3

--- ncurses_install.sh
+++ ncurses_install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+mkdir ./installed
+
 PATH="${TC_TOOLCHAIN_PATH}:${PATH}"
-CC=${TC_TOOLCHAIN_TRIPLET}-gcc  ./configure ${TC_TOOLCHAIN_TRIPLET} --host=x86_64-gnu-linux --target=${TC_TOOLCHAIN_TRIPLET} --with-shared
+CC=${TC_TOOLCHAIN_TRIPLET}-gcc  ./configure ${TC_TOOLCHAIN_TRIPLET} --host=x86_64-gnu-linux --target=${TC_TOOLCHAIN_TRIPLET} --with-shared --prefix=./installed
 
 make HOSTCC=gcc CXX=${TC_TOOLCHAIN_TRIPLET}-c++
 
-#make install
+make install
