6884187882e22e88cb5e35c0888ce4ec6e137e2f
Shavyrin Dmitry <Dmitry.Shavyrin@paragon-software.com>
2015-05-12 15:20:56 +0300

    Editing install script

--- ncurses_install.sh
+++ ncurses_install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-mkdir $(pwd)/ncurses_bin
+root_dir=$(pwd)
+mkdir ${root_dir}/ncurses_bin
+mkdir ${root_dir}/build
+
+cd ${root_dir}/build
 
 PATH="${TC_TOOLCHAIN_PATH}:${PATH}"
-CC=${TC_TOOLCHAIN_TRIPLET}-gcc  ./configure ${TC_TOOLCHAIN_TRIPLET} --host=x86_64-gnu-linux --target=${TC_TOOLCHAIN_TRIPLET} --with-shared --prefix=$(pwd)/ncurses_bin
+CC=${TC_TOOLCHAIN_TRIPLET}-gcc  ${root_dir}/src/configure ${TC_TOOLCHAIN_TRIPLET} --host=x86_64-gnu-linux --target=${TC_TOOLCHAIN_TRIPLET} --with-shared --prefix=${root_dir}/ncurses_bin
 
 make HOSTCC=gcc CXX=${TC_TOOLCHAIN_TRIPLET}-c++
 
 make install
+
+rm -rf ${root_dir}/build
