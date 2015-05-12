7d6518fbb81a58dee330f20b61e00c424240f86e
Shavyrin Dmitry <Dmitry.Shavyrin@paragon-software.com>
2015-05-12 14:56:00 +0300

    Move for TC usage

--- ncurses_install.sh
+++ ncurses_install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir $(pwd)/installed
+mkdir $(pwd)/ncurses_bin
 
 PATH="${TC_TOOLCHAIN_PATH}:${PATH}"
-CC=${TC_TOOLCHAIN_TRIPLET}-gcc  ./configure ${TC_TOOLCHAIN_TRIPLET} --host=x86_64-gnu-linux --target=${TC_TOOLCHAIN_TRIPLET} --with-shared --prefix=$(pwd)/installed
+CC=${TC_TOOLCHAIN_TRIPLET}-gcc  ./configure ${TC_TOOLCHAIN_TRIPLET} --host=x86_64-gnu-linux --target=${TC_TOOLCHAIN_TRIPLET} --with-shared --prefix=$(pwd)/ncurses_bin
 
 make HOSTCC=gcc CXX=${TC_TOOLCHAIN_TRIPLET}-c++
 
